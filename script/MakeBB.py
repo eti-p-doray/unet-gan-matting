6bbb34858a45ae401b49a56450309e286ca8335c
Bettorun <youri.barsalou@gmail.com>
2018-04-03 21:32:43 -0400

    Moving some image manipulations functionalities in functions that could be reused efficiently when the image is already opened

--- script/MakeBB.py
+++ script/MakeBB.py
@@ -3,6 +3,8 @@ import argparse
 from os import listdir
 import os.path
 from PIL import Image, ImageDraw #NB : Requires Pillow : https://pillow.readthedocs.io/en/3.1.x/installation.html
+import image_manips
+
 
 # Taken from https://stackoverflow.com/a/14117511/5128743
 def check_positive(value):
@@ -46,39 +48,15 @@ else:
 for i, target_name in enumerate(target_names):
     # Pillow image object initialisation
     target = Image.open(target_name)
-    output = ImageDraw.Draw(target)
 
-    # Get bounding box (bb) around the non-zero pixels of the target mask
-    bb = target.getbbox() # returns (Left, Up, Right, Down)
-
-    # Fit bounding box to image coordinates
-    bb_coords = [0,0,0,0] # will contain 2 points
-    bb_coords[0] = max(0, bb[0] - args.padding) # Point1 x
-    bb_coords[1] = max(0, bb[1] - args.padding) # Point1 y
-    bb_coords[2] = min(target.size[0], bb[2] + args.padding) # Point2 x
-    bb_coords[3] = min(target.size[1], bb[3] + args.padding) # Point2 y
-
-    # Set color to white
-    whites = { #mode <=> value
-        "1":1, #Binary image, 0 or 1
-        "L":255, "P":255, #grayscale, 0 to 255
-        "RGB":(255,255,255), "HSV":(0,0,255), "CMYK":(0,0,0,0),
-        "RGBA":(255,255,255,255), "LAB":(100, 0.01, -0.01), "YCbCr":(1,0,0)
-    }
-    color = whites.get(target.mode, 16777215) #Default for signed integer
-
-    # Draw bounding box
-    output.rectangle(bb_coords, fill=color, outline=color)
+    target = image_manips.getBoundingBox(target, args.padding)
 
     if args.image is not None:
         # Apply mask upon the -i image and save result
         input_img = Image.open(image_names[i])
-        black = Image.new(input_img.mode, input_img.size) # Creating a new image with the default black background
-        mask = target.convert("1") # transform bounding box image to black and white
-        mask = mask.resize(input_img.size, Image.BICUBIC) #Adjust to input size
-        black.paste(input_img, mask=mask)
-        black.save(output_names[i])
-        black.close()
+        applied_mask = image_manips.applyMask(input_img, target)
+        applied_mask.save(output_names[i])
+        applied_mask.close()
         input_img.close()
 
     else:
